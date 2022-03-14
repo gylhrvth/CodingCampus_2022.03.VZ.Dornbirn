b387e383278bac4a6d052a0cefc2def71b764483
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-03-14 14:14:43 +0100

    My first application - Tasks

--- src/main/java/murat/week01/CountTo10.java
+++ src/main/java/murat/week01/CountTo10.java
@@ -2,21 +2,46 @@ package murat.week01;
 
 public class CountTo10 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("---FOR---");
-		for(int i=0; i <= 10; ++i)
-		{
-			System.out.println("The Number: " + i);
-		}
-		System.out.println("---WHILE---");
+        System.out.println("---TASK 1---");
+        System.out.println("---FOR---");
+        for (int i = 0; i <= 10; ++i) {
+            System.out.println("The Number: " + i);
+        }
+        System.out.println("---WHILE---");
+        int counter = 0;
+        while (counter <= 10) {
+            System.out.println("The Number: " + counter);
+            ++counter;
+        }
 
-		int counter = 0;
-		while (counter <= 10)
-		{
-			System.out.println("The Number: " + counter);
-			++counter;
-		}
-	}
+        System.out.println("------------------------------------");
+        System.out.println("---TASK 2---");
 
+        for (int i = 0; i < 10; ++i) {
+            System.out.println("The Number: " + i);
+        }
+
+        System.out.println("------------------------------------");
+        System.out.println("---TASK 3---");
+        for (int i=0; i <= 10; ++i) {
+            if((i%2)==0) {
+                System.out.println("Even Numbers: " + i);
+            }
+        }
+
+        System.out.println("------------------------------------");
+        System.out.println("---TASK 3-Simpler---");
+        for (int i=0; i <=10; i=i+2)
+        {
+            System.out.println("Even Numbers: " + i);
+        }
+
+        System.out.println("------------------------------------");
+        System.out.println("---TASK 4---");
+        for (int i=10; i >= -10; --i) {
+            System.out.println("The Number: " + i);
+        }
+    }
 }
