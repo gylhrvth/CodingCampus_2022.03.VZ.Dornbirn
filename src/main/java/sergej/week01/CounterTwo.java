3347d9586f0ae080fb5c36ceb3cd62e72ffac7af
Sergej <orasch.s@live.at>
2022-03-14 14:23:31 +0100

    Counter assignment two

---
+++ src/main/java/sergej/week01/CounterTwo.java
@@ -0,0 +1,13 @@
+package sergej.week01;
+
+public class CounterTwo {
+    public static void main(String[] args) {
+
+
+        for (int counter = 0; counter <=10 ; ++counter ){
+            System.out.println("counter:"+ counter);
+
+        }
+
+    }
+}
