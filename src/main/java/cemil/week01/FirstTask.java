1563c97a2fbdf26fdb7c1778627d6793009a8059
Cemil Dincer <cemil_dincer@gmx.at>
2022-03-14 15:30:27 +0100

    My first Operator

--- src/main/java/cemil/week01/FirstTask.java
+++ src/main/java/cemil/week01/FirstTask.java
@@ -3,5 +3,47 @@ package cemil.week01;
 public class FirstTask {
     public static void main(String[] args) {
 
+        int counter1 = 0;
+        int limit = 10;
+        while (counter1 <= limit) {
+            System.out.println(counter1);
+            ++counter1;
+        }
+        System.out.println("-----------------");
+        for (int counter2 = 0; counter2 <= limit; ++counter2) {
+            System.out.println(counter2);
+        }
+        System.out.println("--------------");
+
+        int counter3 = 0;
+        while (counter3 < limit) {
+            System.out.println(counter3);
+            ++counter3;
+
+        }
+        System.out.println("--------------");
+
+        int counter4 = 0;
+        while (counter4 <= limit) {
+            if (counter4 % 2 == 0) {
+                System.out.println(counter4);
+            }
+            ++counter4;
+        }
+
+        System.out.println("--------------");
+
+
+
+        for (int counter5 = 10; counter5 >= -10; counter5 = counter5 -1 ){
+            System.out.println(counter5);
+
+        }
+
+        System.out.println("-------------");
+
+
+
+
     }
 }
