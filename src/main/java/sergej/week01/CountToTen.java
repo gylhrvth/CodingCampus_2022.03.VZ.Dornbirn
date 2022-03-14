68c2eabf3fbf54ea1f5c4f86506b502281f3353b
Sergej <orasch.s@live.at>
2022-03-14 14:16:57 +0100

    Counter assignment one

---
+++ src/main/java/sergej/week01/CountToTen.java
@@ -0,0 +1,20 @@
+package sergej.week01;
+
+public class CountToTen {
+    public static void main(String[] args) {
+        int limit = 11;
+        int counter2 = 0;
+
+        for (int counter = 0; counter < limit; ++counter ){
+            System.out.println(counter);
+
+        }
+
+        while (counter2 < limit) {
+            System.out.println(counter2);
+            counter2 = counter2 + 1;
+        }
+
+
+    }
+}
