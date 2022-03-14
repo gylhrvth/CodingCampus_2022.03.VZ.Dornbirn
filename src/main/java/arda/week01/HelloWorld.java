53d357680384b23908b49dc62a5042a0b1e4a606
aimless-cpu <s.grabher@protonmail.com>
2022-03-14 12:21:58 +0100

    Basic program structure

--- src/main/java/arda/week01/HelloWorld.java
+++ src/main/java/arda/week01/HelloWorld.java
@@ -2,6 +2,39 @@ package arda.week01;
 
 public class HelloWorld {
     public static void main(String[] arg) {
-        System.out.println("Hello World");
+        int number = 3;
+        System.out.println("Hello World " + number);
+
+        // Line Comment
+        /*
+           Block Comment
+        */
+        // Sequence
+        System.out.println(".........\n");
+        System.out.println("Hello ");
+        System.out.println("World\n ");
+
+        // If-Statement
+        if (number > 5) {
+            System.out.println("number is more than 5");
+        } else {
+            System.out.println("number is less or equal");
+
+            //while- and for- Statement
+            int counter = 0;
+            int limit = 5;
+            while (counter < limit){
+                System.out.println(counter + " Arda");
+                ++counter;
+                //counter = counter + 1;
+                //counter += 1;
+            }
+
+            for (int counter2 = 0; counter2 < limit; ++counter2){
+                System.out.print("A");
+            }
+            System.out.println();
+        }
+
     }
 }
