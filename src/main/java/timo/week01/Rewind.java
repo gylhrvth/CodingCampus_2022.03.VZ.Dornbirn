f563e18fb8c9c474e4a4bdd4164b9b228093f5d7
timokantner <timo.kantner@chello.at>
2022-03-14 15:31:40 +0100

    progress

--- src/main/java/timo/week01/Rewind.java
+++ src/main/java/timo/week01/Rewind.java
@@ -5,7 +5,7 @@ public class Rewind {
 
         int limit = -10;
 
-        for (int counter = 10; counter <= -10; --counter) {
+        for (int counter = 10; counter < 10; --counter) {
             if (counter <= limit)
             System.out.println(counter);
         }
