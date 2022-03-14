1d9e01dcb210cb4faf8181418fd0fcbcd9f8bf5f
Razvan <zah_razvan@yahoo.com>
2022-03-14 12:22:06 +0100

    My first application

--- src/main/java/zah/week01/HelloWorld.java
+++ src/main/java/zah/week01/HelloWorld.java
@@ -2,7 +2,42 @@ package zah.week01;
 
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        int number = 3;
+        System.out.println(" Hello World! " + number);
+
+        //line comment
+        /*
+        Block Comment
+         */
+        // Sequence
+        System.out.print(" ___________\n ");
+        System.out.print(" Hello " );
+        System.out.print(" World \n");
+        // If-Statement
+        if(number >5){
+            System.out.println(" Number is more that 5");
+        } else{
+            System.out.println( " Number is less or equal 5");
+        }
+
+        // while- and for Statement
+        int counter = 0;
+        int limit = 5;
+        while (counter < limit){
+            System.out.println(counter + " Razvan ");
+            ++ counter;
+            //counter + =1;
+            //counter = counter + 1;
+
+
+
+
+        }
+        for (int counter2 =0; counter2 < limit; ++counter2){
+            System.out.print( "A");
+        }
+        System.out.println();
+
     }
 
 }
