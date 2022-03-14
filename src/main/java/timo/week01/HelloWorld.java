127315e532bb2d3c3671a4707eefe5ef4532796d
timokantner <timo.kantner@chello.at>
2022-03-14 14:14:51 +0100

    Aufgabe: Zählen

--- src/main/java/timo/week01/HelloWorld.java
+++ src/main/java/timo/week01/HelloWorld.java
@@ -2,6 +2,40 @@ package timo.week01;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int number = 4;
+        System.out.println("Hello World! " + number);
+
+        // Line comment
+
+        /*
+        Block comment
+         */
+
+        // Sequence
+        System.out.println("-------------");
+        System.out.println("Hello ");
+        System.out.println("World!");
+
+
+        // If-Statement
+        if (number > 5) {
+            System.out.println("Number is more than 5");
+        } else {
+            System.out.println("Number is less or equal 5");
+        }
+
+        // while- and for-Statement
+        int counter = 0;
+        int limit = 5;
+        while (counter < limit) {
+            System.out.println(counter + " Timo");
+            counter = counter + 1;
+            //counter +=1 or ++counter
+        }
+
+        for (int counter2 = 0; counter2 < limit; ++counter2) {
+            System.out.println("A");
+        }
+        System.out.println();
     }
 }
---
+++ src/main/java/timo/week01/LoopAssignment.java
@@ -0,0 +1,20 @@
+package timo.week01;
+
+public class LoopAssignment {
+    public static void main(String[] args) {
+
+        int limit = 11;
+        int counter2 = 0;
+
+        for (int counter = 0; counter < limit; ++counter) {
+            System.out.println(counter);
+        }
+        System.out.println("----------------------------");
+
+        while (counter2 < limit)
+        {
+            System.out.println(counter2);
+            ++counter2;
+        }
+    }
+}
