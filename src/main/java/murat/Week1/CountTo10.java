dbd3531c626f16caec090350612fad489d1e7c0b
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-03-15 10:05:49 +0100

    My first application

--- src/main/java/murat/Week1/CountTo10.java
+++ src/main/java/murat/Week1/CountTo10.java
@@ -2,11 +2,12 @@ package murat.Week1;
 
 public class CountTo10 {
 
-	public static void task1(String name, int limit) {
+	// Task 1
+	public static void task1(String name, int start, int limit) {
 
 		System.out.println("---" + name + "---");
 		System.out.println("---FOR---");
-		for (int i = 0; i <= limit; ++i) {
+		for (int i = start; i <= limit; ++i) {
 			System.out.println("The Number: " + i);
 		}
 		System.out.println("---WHILE---");
@@ -18,50 +19,54 @@ public class CountTo10 {
 		System.out.println("------------------------------------");
 	}
 
-	public static void task2(String name, int limit) {
+	// Task 2
+	public static void task2(String name, int start, int limit) {
 
 		System.out.println("---TASK 2---");
-		for (int i = 0; i < limit; ++i) {
+		for (int i = start; i < limit; ++i) {
 			System.out.println("The Number: " + i);
 		}
 	}
 
-	public static void task3(String name, int limit) {
+	// Task 3 mit Modulo
+	public static void task3(String name, int start, int limit, int divisor) {
 
 		System.out.println("------------------------------------");
 		System.out.println("---TASK 3---");
-		for (int i = 0; i <= limit; ++i) {
-			if ((i % 2) == 0) {
+		for (int i = start; i <= limit; ++i) {
+			if ((i % divisor) == 0) {
 				System.out.println("Even Numbers: " + i);
 			}
 		}
 	}
 
-	public static void task3Simpler(String name, int limit) {
+	// Task 3 Simpler
+	public static void task3Simpler(String name, int start, int limit) {
 
 		System.out.println("------------------------------------");
 		System.out.println("---TASK 3-Simpler---");
-		for (int i = 0; i <= limit; i = i + 2) {
+		for (int i = start; i <= limit; i = i + 2) {
 			System.out.println("Even Numbers: " + i);
 		}
 	}
 
-	public static void task4(String name, int limit) {
+	// Task 4
+	public static void task4(String name, int start, int limit) {
 
 		System.out.println("------------------------------------");
 		System.out.println("---TASK 4---");
-		for (int i = 10; i >= limit; --i) {
+		for (int i = start; i >= limit; --i) {
 			System.out.println("The Number: " + i);
 		}
 	}
 
 	public static void main(String[] args) {
 
-		task1("TASK 1", 10);                    // Count to 10 with FOR and WHILE
-		task2("TASK 2", 10);                    // Count to 10 with FOR exclusively 10
-		task3("TASK 3", 10);                    // Count to 10 and display the even numbers, Modulo Method
-		task3Simpler("TASK 3-Simpler", 10);     // Display the even numbers between 0-10
-		task4("TASK 4", -10);                   // Count backwards from 10 to -10 and display
+		task1("TASK 1", 0,10);                    // Count to 10 with FOR and WHILE
+		task2("TASK 2", 0,10);                    // Count to 10 with FOR exclusively 10
+		task3("TASK 3", 0,10, 2);                    // Count to 10 and display the even numbers, Modulo Method
+		task3Simpler("TASK 3-Simpler", 0,10);     // Display the even numbers between 0-10
+		task4("TASK 4", 10,-10);                   // Count backwards from 10 to -10 and display
 	}
 
 }
--- src/main/java/murat/Week1/DiffBetwPlusPlusCounterUndCounterPlusPlus.java
+++ src/main/java/murat/Week1/DiffBetwPlusPlusCounterUndCounterPlusPlus.java
@@ -10,6 +10,19 @@ public class DiffBetwPlusPlusCounterUndCounterPlusPlus {
 		System.out.println("Number 1: " + ++number1 + " " + "Number 2: " + number2++);
 		System.out.println("Number 1: " + number1 + " " + "Number 2: " + number2);
 
+		System.out.println("-----------------------------------------------------------");
+
+		int testNumber1 = 0;
+		int testNumber2 = 0;
+		number1 = 1;
+		number2 = 1;
+		++number1;
+		number2++;
+		testNumber1 = number1;
+		testNumber2 = number2;
+		System.out.println("TestNumber 1: " + testNumber1 + " " + "TestNumber 2: " + testNumber2);
+		System.out.println("Number 1: " + number1 + " " + "Number 2: " + number2);
+
 	}
 
 }
