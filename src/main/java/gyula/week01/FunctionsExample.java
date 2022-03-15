f36694659f0164de698753dce86e6b904ee50b49
Gyula Horvath <gylhrvth@gmail.com>
2022-03-15 10:05:17 +0100

    Function examples

---
+++ src/main/java/gyula/week01/FunctionsExample.java
@@ -0,0 +1,30 @@
+package gyula.week01;
+
+public class FunctionsExample {
+    public static void main(String[] args) {
+        int testValue = 7;
+        task1("Beispiel 1", testValue);
+        task1("AAAA", 10);
+
+        testValue = doubleInt(3);
+    }
+
+    public static int doSomething(){
+        System.out.println(1+2);
+        return 1+2;
+    }
+
+    public static int doubleInt(int number){
+        System.out.println(2*number);
+        return 2*number;
+    }
+
+    public static void task1(String name, int limit){
+        System.out.println(name);
+        for (int i = 0; i < limit; i++) {
+            System.out.println(i);
+        }
+        System.out.println("========================");
+    }
+
+}
---
+++ src/main/java/gyula/week01/FunctionsExample2.java
@@ -0,0 +1,7 @@
+package gyula.week01;
+
+public class FunctionsExample2 {
+    public static void main(String[] args) {
+        FunctionsExample.task1("Hello", 5);
+    }
+}
