03215a51b0eec4b73ec38b3f1dc80fa56e0e5b04
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-03-15 08:56:13 +0100

    My first application

--- src/main/java/murat/Week1/CountTo10.java
+++ src/main/java/murat/Week1/CountTo10.java
@@ -2,8 +2,9 @@ package murat.Week1;
 
 public class CountTo10 {
 
-	public static void task1(String name, int limit){
-		System.out.println("---"+ name +"---");
+	public static void task1(String name, int limit) {
+
+		System.out.println("---" + name + "---");
 		System.out.println("---FOR---");
 		for (int i = 0; i <= limit; ++i) {
 			System.out.println("The Number: " + i);
@@ -17,14 +18,16 @@ public class CountTo10 {
 		System.out.println("------------------------------------");
 	}
 
-	public static void task2 (String name, int limit){
+	public static void task2(String name, int limit) {
+
 		System.out.println("---TASK 2---");
 		for (int i = 0; i < limit; ++i) {
 			System.out.println("The Number: " + i);
 		}
 	}
 
-	public static void task3 (String name, int limit){
+	public static void task3(String name, int limit) {
+
 		System.out.println("------------------------------------");
 		System.out.println("---TASK 3---");
 		for (int i = 0; i <= limit; ++i) {
@@ -34,7 +37,8 @@ public class CountTo10 {
 		}
 	}
 
-	public static void task3Simpler (String name, int limit){
+	public static void task3Simpler(String name, int limit) {
+
 		System.out.println("------------------------------------");
 		System.out.println("---TASK 3-Simpler---");
 		for (int i = 0; i <= limit; i = i + 2) {
@@ -42,7 +46,8 @@ public class CountTo10 {
 		}
 	}
 
-	public static void task4 (String name, int limit){
+	public static void task4(String name, int limit) {
+
 		System.out.println("------------------------------------");
 		System.out.println("---TASK 4---");
 		for (int i = 10; i >= limit; --i) {
@@ -51,10 +56,12 @@ public class CountTo10 {
 	}
 
 	public static void main(String[] args) {
+
 		task1("TASK 1", 10);                    // Count to 10 with FOR and WHILE
 		task2("TASK 2", 10);                    // Count to 10 with FOR exclusively 10
 		task3("TASK 3", 10);                    // Count to 10 and display the even numbers, Modulo Method
 		task3Simpler("TASK 3-Simpler", 10);     // Display the even numbers between 0-10
 		task4("TASK 4", -10);                   // Count backwards from 10 to -10 and display
 	}
+
 }
---
+++ src/main/java/murat/Week1/DiffBetwPlusPlusCounterUndCounterPlusPlus.java
@@ -0,0 +1,15 @@
+package murat.Week1;
+
+public class DiffBetwPlusPlusCounterUndCounterPlusPlus {
+
+	public static void main(String[] args) {
+
+		int number1 = 1;
+		int number2 = 1;
+		System.out.println("Number 1: " + number1 + " " + "Number 2: " + number2);
+		System.out.println("Number 1: " + ++number1 + " " + "Number 2: " + number2++);
+		System.out.println("Number 1: " + number1 + " " + "Number 2: " + number2);
+
+	}
+
+}
