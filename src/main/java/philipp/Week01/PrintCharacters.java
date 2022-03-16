b92d78ea6f20df19be3650e02fc396545f3f2a52
PhilippRB <philbert@gmx.at>
2022-03-16 17:52:26 +0100

    PrintCharacters exercises  16.03.2022

--- src/main/java/philipp/Week01/PrintCharacters.java
+++ src/main/java/philipp/Week01/PrintCharacters.java
@@ -3,6 +3,7 @@ package philipp.Week01;
 public class PrintCharacters {
     public static void main(String[] args) {
         //printChars("A ", 3);
+        //printChars2("A ", 3);
         //printSquare("B ", 5);
         //printRect("C ", 5, 2);
         //printTriangleBottomLeft("D ", 4);
@@ -13,11 +14,28 @@ public class PrintCharacters {
         //System.out.println();
         //printTriangleBottomRight("G ", 5);
         //System.out.println();
-        printEmptySquare("H ", 10);
+        //printEmptySquare("H ", 10);
+        //System.out.println();
+        printEmptySquare("I ", 5);
         System.out.println();
+        printEmptySquare2("I ", 5);
+        System.out.println();
+        printSlash("J ", 3, true);
+        System.out.println();
+        printSlash("K ", 4, false);
+        System.out.println();
+        //printTriangle("L ", 3);
+        //System.out.println();
     }
 
     public static void printChars(String name, int amount) {
+        for (int i = 0; i < amount; i++) {
+            System.out.print(name);
+        }
+        System.out.println();
+    }
+
+    public static void printChars2(String name, int amount) {
         int i = 1;
         while (i <= amount) {
             System.out.print(name);
@@ -89,14 +107,14 @@ public class PrintCharacters {
 
     public static void printEmptySquare(String x, int size) {
         for (int i = 0; i < size; i++) {
-            if (i == 0 || i==size-1) {
+            if (i == 0 || i == size - 1) {
                 for (int j = 0; j < size; j++) {
                     System.out.print(x);
                 }
             } else {
                 for (int j = 0; j < size; j++) {
                     if (j > 0 && j < size - 1) {
-                        System.out.print("* ");
+                        System.out.print("  ");
                     } else {
                         System.out.print(x);
                     }
@@ -105,7 +123,65 @@ public class PrintCharacters {
             System.out.println();
         }
     }
+
+    public static void printEmptySquare2(String x, int size) {
+        printChars(x, size);
+        for (int i = 0; i < size - 2; i++) {
+            for (int j = 0; j < size; j++) {
+                if (j > 0 && j < size - 1) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(x);
+                }
+            }
+            System.out.println();
+        }
+        printChars(x, size);
+    }
+
+
+    public static void printSlash(String x, int size, boolean b) {
+        if (b) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (j != i) {
+                        System.out.print("  ");
+                    } else {
+                        System.out.print(x);
+                    }
+                }
+                System.out.println();
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (j != size - 1 - i) {
+                        System.out.print("  ");
+                    } else {
+                        System.out.print(x);
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static void printTriangle(String x, int size) {
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j < size * 2 - 1; j++) {
+                if (i == size * 2 - 1) {
+                    System.out.print(x);
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        printChars(x, size * 2 - 1);
+    }
 }
+
+
 
 
 
