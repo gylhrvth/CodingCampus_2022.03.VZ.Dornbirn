e973f3f701cef475e3a36e9dec07e0565c389cda
timokantner <timo.kantner@chello.at>
2022-03-16 13:38:31 +0100

    added empty square

--- src/main/java/timo/week01/MethodenSchleifen.java
+++ src/main/java/timo/week01/MethodenSchleifen.java
@@ -6,17 +6,20 @@ public class MethodenSchleifen {
 
     public static void main(String[] args) {
         //printChars("x ", 5);
-        //printSquare("A ", 4);
-        //printRectangle("B ", 10, 3);
-        //printTriangleBottomLeft("C ", 4);
-        printTriangleTopLeft("D ", 5);
+        //printSquare("x ", 4);
+        //printRectangle("x ", 10, 3);
+        //printTriangleBottomLeft("x ", 4);
+        //printTriangleTopLeft("x ", 5);
+        //printTriangleBottomRight("x", 5);
+        printEmptySquare("x", 10);
 
 
     }
+
     public static void printChars(String letter, int number) {
 
         for (int i = 0; i < number; i++) {
-        System.out.print(letter);
+            System.out.print(letter);
         }
         System.out.println();
     }
@@ -54,10 +57,33 @@ public class MethodenSchleifen {
     public static void printTriangleTopLeft(String letter, int size) {
 
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size-i; j++) {
+            for (int j = 0; j < size - i; j++) {
                 System.out.print(letter);
             }
             System.out.println();
+        }
+    }
+
+    public static void printTriangleBottomRight(String letter, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print(letter);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printEmptySquare(String letter, int size) {
+        for (int i = 0; i < size; i++) {
+            if (i == 1 || i == size-1) {
+                System.out.println("--------");
+                }
+            for (int j = 0; j <= size; j++) {
+            }
+            System.out.println(letter);
         }
     }
 }
