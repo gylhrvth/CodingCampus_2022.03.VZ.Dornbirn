a8283e4b05c7144fe57a688beba916a29f5b04da
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-03-20 00:14:27 +0100

    Print Circle with Pythagoras --> Lower semicircle is done.

--- src/main/java/murat/week1/MethodsAndLoops.java
+++ src/main/java/murat/week1/MethodsAndLoops.java
@@ -278,31 +278,29 @@ public class MethodsAndLoops {
 
     // Task 14 - Print Circle with Pythagoras
     public static void printCirclePythagoras(int size) {
+        /* I divided the circle into 2 semicircles as upper and lower part.
+        Then i created the full cells.
+        Pushing the empty cells to the right gives us a semicircle */
 
         System.out.println("Task 14 - Print Circle with Pythagoras");
+        int newSize = size * 2;    // Size is radius. An upper part requires 2*size in with or X Axis
+        int xAxis;          // Width
+        double yAxis;       // Height
 
-        int newSize = size * 2;    // Circle size is 1 line less than the original size
-
-        double xAxis;
-        double yAxis;
-        System.out.println("Size: " + size);
-
-        for (int i = 0; i < newSize; i++) {
-            yAxis = i;
+        for (int i = 1; i <= newSize / 2; i++) {
+            yAxis = i;           // Top to the bottom, increased by 1
 
             // if X axis increases, y axis should be less in accordance with Pytagoras
-            int diverge = (int) (Math.sqrt(newSize * newSize - yAxis * yAxis));
-            for (int j = 0; j < diverge; ++j) {
+            // Rounding the X Axis (Width) to the nearest number.
+            xAxis = (int) Math.round(Math.sqrt(size * size - yAxis * yAxis));
 
-                //System.out.print(" i" + i + " j" + j);
-                //System.out.print(" A" + i + j);
+            // Empty cells push the full cells to the right
+            for (int k = 0; k < (size - xAxis); ++k) {
+                System.out.print(" ");
+            }
 
-                // TEST if each horizontal line is less than size, than leave a space
-               /* if (diverge < newSize) {
-                    for (int k = 0; k < newSize-diverge; k++){
-                        System.out.print("");
-                    }
-                }*/
+            // Full cells
+            for (int j = 1; j <= xAxis * 2; ++j) {
                 System.out.print("A");
             }
             System.out.println();
@@ -321,9 +319,9 @@ public class MethodsAndLoops {
         // printSlash("M", 3, false);                    // Task 9 - Print Slash
         // printTriangle("M", 7);                        // Task 10 - Print Triangle
         // printRhombus("M", 101);                       // Task 11 - Print Rhombus
-        // printX("M", 5);                               // Task 12 - Print X
+        // printX("M", 10);                               // Task 12 - Print X
         // printChristmasTree(15);                       // Task 13 - Print Christmas Tree
-        printCirclePythagoras(4);
+        printCirclePythagoras(20);
     }
 }
 
