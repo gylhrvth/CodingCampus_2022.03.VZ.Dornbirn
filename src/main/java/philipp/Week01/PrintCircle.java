f3e4e92873c78ec3f229bc37c3e38327b850ea2e
PhilippRB <philbert@gmx.at>
2022-03-20 21:19:29 +0100

    Circle Pythagoras working

--- src/main/java/philipp/Week01/PrintCircle.java
+++ src/main/java/philipp/Week01/PrintCircle.java
@@ -9,17 +9,16 @@ public class PrintCircle {
 
     public static void printCirclePythagoras(int radius) {
         //a2+b2=c2
-        for (int i = -radius; i < radius; i++) {
-            for (int j = -radius; j < radius; j++) {
-                double a = Math.sqrt((radius * radius) - (radius - i) * (radius - i));
-                double b = Math.sqrt((radius * radius) - (radius - j) * (radius - j));
+        for (int i = 0; i < radius * 2; i++) {
+            for (int j = 0; j < radius * 2; j++) {
+                double a = Math.sqrt((radius * radius) - ((radius - i) * (radius - i)));
+                double b = Math.sqrt((radius * radius) - ((radius - j) * (radius - j)));
 
-                if ((radius + radius) == (a * a) + (b * b)) {
+                if ((radius * radius) < (a * a) + (b * b)) {
                     System.out.print("X");
                 } else {
-                    System.out.print("O");
+                    System.out.print(" ");
                 }
-
             }
             System.out.println();
         }
