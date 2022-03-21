dd25d88a842202f3dae0abd3ea1775380bb708f9
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-03-21 14:21:37 +0100

    Task 4: Calculation of Pi with Nikalantha is done

---
+++ src/main/java/murat/week2/day1/dataTypesTask4CalculatingPiWithNikalanthaVersion2.java
@@ -0,0 +1,23 @@
+package murat.week2.day1;
+
+public class dataTypesTask4CalculatingPiWithNikalanthaVersion2 {
+
+    public static void main(String[] args) {
+
+        calculatingPiNilakantha(5);
+    }
+
+    public static void calculatingPiNilakantha(double min) {
+
+        double muratPi = 3;
+        double converge = 10;
+
+        while (converge > min) {
+            System.out.println("M");
+
+            for (int i)
+            converge = converge + 4
+        }
+
+    }
+}
