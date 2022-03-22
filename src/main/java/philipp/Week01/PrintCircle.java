e369d64c794593e2da920f467997ec670d73e1a3
PhilippRB <philbert@gmx.at>
2022-03-22 08:35:07 +0100

    DataTypeVariables and Exercises Circle with distance 2.6

--- src/main/java/philipp/Week01/PrintCircle.java
+++ src/main/java/philipp/Week01/PrintCircle.java
@@ -5,13 +5,28 @@ public class PrintCircle {
     public static void main(String[] args) {
         printCirclePythagoras(10);
         System.out.println();
+        printCirclePythagoras2(10);
+        System.out.println();
     }
 
         public static void printCirclePythagoras(int radius) {
         //a2+b2=c2
         for (int i = 0; i < radius * 2; i++) {
             for (int j = 0; j < radius * 2; j++) {
-                if ((radius * radius) > ((radius - i) * (radius - i)) + ((radius - j) * (radius - j))) {
+                if ((radius * radius) > ((radius - j) * (radius - j)) + (((radius - i)*2.6) * ((radius - i)*2.6))) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void printCirclePythagoras2(int radius) {
+        //a2+b2=c2
+        for (int i = 0; i < radius * 2; i++) {
+            for (int j = 0; j < radius * 2; j++) {
+                if ((radius * radius) > ((radius - j) * (radius - j)) + ((radius - i) * (radius - i))) {
                     System.out.print("X");
                 } else {
                     System.out.print(" ");
---
+++ src/main/java/philipp/Week02/DataTypeVariables.java
@@ -0,0 +1,140 @@
+package philipp.Week02;
+
+public class DataTypeVariables {
+    public static void main(String[] args) {
+
+        intVariables();
+        floatingPointVariables();
+        System.out.println();
+        ZeroToHundred();
+        System.out.println();
+        DivideEtImpera();
+        System.out.println();
+        Leibniz();
+        System.out.println();
+        Nilakantha();
+        System.out.println();
+        SecretRow();
+        System.out.println();
+        GuessingGame(9);
+    }
+
+    public static void intVariables() {
+
+        //integer variables
+        byte a = 0; // 8 Bit
+        short b = 0; //16 Bit
+        int c = 0; //32 Bit
+        long d = 0; //64 Bit
+
+        System.out.println(100);
+        a = 100;
+        d = 100L;
+        // a = 100L; Does not work!
+        a = (byte) 100L;
+        d = 123456789L;
+        a = (byte) d;
+        System.out.println(d + " " + a);
+
+        System.out.println(2 + a);
+        System.out.println(a - 10);
+        System.out.println(a * 5);
+        System.out.println(d / 1234);
+        System.out.println(d % 1234);
+
+    }
+
+    public static void floatingPointVariables() {
+        // +/- 3.456789654 (*10^ +/- 4)
+        // 3.456 *10^4 ==> 34560
+        // 8.112*10^-3 ==> 0.008112
+
+        float fVariable = (float) 2.25;
+        fVariable = 1.55f;
+        double dVariable = 2.25;
+
+        System.out.println(2 + fVariable);
+        System.out.println(fVariable - 10);
+        System.out.println(fVariable * 5);
+        System.out.println(fVariable / 1234);
+
+        double testVariable = 15f;
+        System.out.println((testVariable / 3.0) * 3.0);
+    }
+
+    public static void ZeroToHundred() {
+        for (double i = 0; i <= 1; i += 0.1) {
+            System.out.println(i);
+        }
+        System.out.println();
+        for (int j = 0; j <= 10; j++) {
+            System.out.println(j / 10f);
+        }
+    }
+
+    public static void DivideEtImpera() {
+        System.out.println(4);
+        System.out.println(4 / 3f);
+        System.out.println(4 / 5f);
+        System.out.println(4 / 7f);
+    }
+
+    public static void Leibniz() {
+        double myPi = 0.0;
+        int denotiator = 1;
+        for (int i = 0; i < 10000000; i++) {
+            if (i % 2 == 0) {
+                myPi = myPi + (4.0 / denotiator);
+            } else {
+                myPi = myPi - (4.0 / denotiator);
+            }
+            denotiator = denotiator + 2;
+        }
+        System.out.println(myPi);
+
+    }
+
+    public static void Nilakantha() {
+        double myPi = 3.0;
+        int j = 2;
+        for (int i = 0; i < 100000; i++) {
+            if (i % 2 == 0) {
+                myPi = myPi + (4.0 / (j * (j + 1) * (j + 2)));
+            } else {
+                myPi = myPi - (4.0 / (j * (j + 1) * (j + 2)));
+            }
+            j = j + 2;
+        }
+        System.out.println(myPi);
+    }
+
+    public static void SecretRow() {
+        double v = 1.0;
+        for (int i = 0; i < 10; i++) {
+            v = (v / 2) + (1 / v);
+            System.out.println(v);
+        }
+    }
+
+    public static void GuessingGame(double input) {
+        for (int i = 0; i < 100000; i++) {
+            double w = input / 2.0;
+
+            if (w * w == input) {
+                System.out.println(w);
+
+            } else if (w * w > input) {
+                input = w;
+            } else {
+                w = 1+w;
+            }
+            System.out.println(w);
+        }
+    }
+
+}
+
+
+
+
+
