73bc191149a9210ddacca64ba40da7ad9f5d2638
Gyula Horvath <gylhrvth@gmail.com>
2022-03-22 11:53:58 +0100

    Example for reading a number from Console

---
+++ src/main/java/gyula/week02/Input.java
@@ -0,0 +1,21 @@
+package gyula.week02;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = Integer.MIN_VALUE;
+
+        System.out.println("Enter a decimal number:");
+        while (value == Integer.MIN_VALUE) {
+            try {
+                String text = sc.nextLine();
+                value = Integer.parseInt(text);
+            } catch (NumberFormatException nfe) {
+                System.out.println("A number is requested");
+            }
+        }
+        System.out.println("The value is " + value);
+    }
+}
