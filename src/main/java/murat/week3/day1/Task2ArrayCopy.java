6bcb6c48f7a2847591e872ab08f6a0f164cc789a
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-03-28 11:18:04 +0200

    Task 2 - simplified

--- src/main/java/murat/week3/day1/Task2ArrayCopy.java
+++ src/main/java/murat/week3/day1/Task2ArrayCopy.java
@@ -20,35 +20,36 @@ public class Task2ArrayCopy {
         System.out.println();
         System.out.println("Copy Array");
         for (int j = 0; j < size; j++) {
-            System.out.print(j + "No " + makeACopy(originalArray, size)[j] + " ");
+            System.out.print(j + "No " + makeACopy(originalArray)[j] + " ");
         }
 
         System.out.println();
         System.out.println();
         System.out.println("Make an array copy with arraycopy");
 
+        int[] copyOfArray = makeACopyWithArrayCopy(originalArray);
         for (int j = 0; j < size; j++) {
-            System.out.print(j + "No " + makeACopyWithArrayCopy(originalArray, size)[j] + " ");
+            System.out.print(j + "No " + copyOfArray[j] + " ");
         }
 
         System.out.println();
     }
 
-    public static int[] makeACopy(int[] anOriginalArray, int size) {
+    public static int[] makeACopy(int[] anOriginalArray) {
 
-        int[] aCopyArray = new int[size];
+        int[] aCopyArray = new int[anOriginalArray.length];
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < aCopyArray.length; i++) {
             aCopyArray[i] = anOriginalArray[i];       // Copy the values
         }
         return aCopyArray;
     }
 
-    public static int[] makeACopyWithArrayCopy(int[] anOriginalArray, int size) {
+    public static int[] makeACopyWithArrayCopy(int[] anOriginalArray) {
 
-        int[] aCopyArray = new int[size];
+        int[] aCopyArray = new int[anOriginalArray.length];
 
-        System.arraycopy(anOriginalArray, 0, aCopyArray, 0, size);
+        System.arraycopy(anOriginalArray, 0, aCopyArray, 0, anOriginalArray.length);
         return aCopyArray;
     }
 }
