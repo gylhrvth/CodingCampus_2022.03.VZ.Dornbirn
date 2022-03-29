962c09531e364e6822d8b97302de5c7c5cadd4e8
Sergej <orasch.s@live.at>
2022-03-29 14:47:02 +0200

    Added Selectionsort Algo

---
+++ src/main/java/sergej/week03/SelectionSort.java
@@ -0,0 +1,31 @@
+package sergej.week03;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            int swap = array[min];
+            array[min] = array[i];
+            array[i] = swap;
+
+
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+
+        int[] newList = RandomArray.randomList(10000);
+        selectionSort(newList);
+
+    }
+}
