a83d597e62404dee4b9e73685faf849e721e6555
Sergej <orasch.s@live.at>
2022-03-30 10:15:52 +0200

    Added CocktailShaker, cheers!

--- src/main/java/sergej/week03/GnomeSort.java
+++ src/main/java/sergej/week03/GnomeSort.java
@@ -3,16 +3,16 @@ package sergej.week03;
 import java.util.Arrays;
 
 public class GnomeSort {
-    public static void gnomeSort(int[] array){
+    public static void gnomeSort(int[] array) {
         int i = 1;
-        while(i< array.length){
-            if(array[i]>array[i-1]){
+        while (i < array.length) {
+            if (array[i] > array[i - 1]) {
                 i++;
-            }else{
+            } else {
                 int swap = array[i];
-                array[i] = array[i-1];
-                array[i-1] = swap;
-                if( i > 1){
+                array[i] = array[i - 1];
+                array[i - 1] = swap;
+                if (i > 1) {
                     i--;
                 }
 
@@ -22,7 +22,7 @@ public class GnomeSort {
 
     public static void main(String[] args) {
 
-        int[] newList = RandomArray.randomList(10);
+        int[] newList = RandomArray.randomList(0);
 
         gnomeSort(newList);
         System.out.println(Arrays.toString(newList));
