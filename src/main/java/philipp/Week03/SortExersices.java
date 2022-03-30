3d1f4d78be6b9a0766f17dc0efa762755e9113d3
PhilippRB <philbert@gmx.at>
2022-03-30 21:44:56 +0200

    Bubble sort and other sort Exercises renamed to SortExersices

--- src/main/java/philipp/Week03/SortExersices.java
+++ src/main/java/philipp/Week03/SortExersices.java
@@ -1,31 +1,37 @@
 package philipp.Week03;
 
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Random;
 
-public class Bubblesort {
+public class SortExersices {
     static Random rn = new Random();
 
     public static void main(String[] args) {
         int[] initArray = randomNo(9900, 10000001);
-        sortImproved(initArray, false);
-
-        int[] testArray = new int[initArray.length + 100];
-        for (int i = 0; i < initArray.length; i++) {
-            testArray[i] = initArray[i];
-        }
-        for (int i = initArray.length; i < testArray.length ; i++) {
-            testArray[i] = rn.nextInt(10000001);
-        }
+        int[] testArray = randomNo(10, 101);
+//        sortImproved(initArray, false);
+//
+//        int[] testArray = new int[initArray.length + 100];
+//        for (int i = 0; i < initArray.length; i++) {
+//            testArray[i] = initArray[i];
+//        }
+//        for (int i = initArray.length; i < testArray.length; i++) {
+//            testArray[i] = rn.nextInt(10000001);
+//        }
 
         System.out.println("bubble");
         System.out.println(Arrays.toString(testArray));
 
-        int[] bubble = testArray.clone();
-        sortImproved(bubble, true);
-        System.out.println(Arrays.toString(bubble));
+        int[] bubbleAsc = testArray.clone();
+        sortImproved(bubbleAsc, true);
+        System.out.println(Arrays.toString(bubbleAsc));
         System.out.println();
+//        int[] bubbleDes = testArray.clone();
+//        sortImproved(bubbleDes, false);
+//        System.out.println(Arrays.toString(bubbleDes));
+//        System.out.println();
+
+
         System.out.println("gnome");
 
         int[] gnome = testArray.clone();
@@ -40,6 +46,21 @@ public class Bubblesort {
         selectionSort(select, select.length);
         System.out.println(Arrays.toString(select));
         System.out.println();
+        System.out.println("cocktail");
+
+        int[] cocktail = testArray.clone();
+        System.out.println(Arrays.toString(cocktail));
+        cocktailShakerSort(cocktail);
+        System.out.println(Arrays.toString(cocktail));
+        System.out.println();
+
+        System.out.println("insertion");
+
+        int[] insertion = testArray.clone();
+        System.out.println(Arrays.toString(insertion));
+        insertionSort(insertion, insertion.length);
+        System.out.println(Arrays.toString(insertion));
+        System.out.println();
 
 //        sortImproved(testArray, false);
 //        System.out.println(Arrays.toString(testArray));
@@ -47,6 +68,23 @@ public class Bubblesort {
 
 //        selectionSort(testArray);
 //        System.out.println(Arrays.toString(testArray));
+    }
+
+
+
+    private static void insertionSort(int[] data, int n) {
+        int countOfCompare = 0;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j > 0 && (data[j - 1] > data[j])) {
+                countOfCompare++;
+                int temp = data[j - 1];
+                data[j - 1] = data[j];
+                data[j] = temp;
+                j = j - 1;
+            }
+        }
+        System.out.println("INFO: CountOfCompare is " + countOfCompare);
     }
 
     private static void gnomeSort(int[] data) {
@@ -69,8 +107,33 @@ public class Bubblesort {
     }
 
     private static void cocktailShakerSort(int[] data) {
-
-
+        int countOfCompare = 0;
+        int lo = 0;
+        int hi = data.length - 1;
+        int mov = lo;
+        while (lo < hi) {
+            for (int i = hi; i > lo; i--) {
+                countOfCompare++;
+                if (data[i - 1] > data[i]) {
+                    int temp = data[i - 1];
+                    data[i - 1] = data[i];
+                    data[i] = temp;
+                    mov = i;
+                }
+            }
+            lo = mov;
+            for (int j = lo; j < hi; j++) {
+                countOfCompare++;
+                if (data[j] > data[j + 1]) {
+                    int temp2 = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp2;
+                    mov = j;
+                }
+            }
+            hi = mov;
+        }
+        System.out.println("INFO: CountOfCompare is " + countOfCompare);
     }
 
     private static void selectionSort(int[] data, int n) {
