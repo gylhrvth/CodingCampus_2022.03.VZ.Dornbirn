4c13a5f6510436eeb7df951d929b15bca4b35817
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-04-04 15:15:02 +0200

    Task 3: "Workdays in a given month" is done.

--- src/main/java/murat/week4/day1/CalendarTasks.java
+++ src/main/java/murat/week4/day1/CalendarTasks.java
@@ -10,11 +10,11 @@ public class CalendarTasks {
         GregorianCalendar gc = new GregorianCalendar(2022, 1, 5);
 
         int userYear = 2021;
-        int userMonth = 10;
-        int userDay = 5;
+        int userMonth = 2;
 
-        //task1NextSunday(gc);
-        task2HowManySundays(userYear, userMonth);
+        // task1NextSunday(gc);
+        // task2HowManySundays(userYear, userMonth);
+        task3WorkdaysInAMonth(userYear, userMonth);
     }
 
     public static void task1NextSunday(GregorianCalendar gc) {
@@ -25,21 +25,16 @@ public class CalendarTasks {
 
     }
 
-    /**
-     * @param year
-     * @param month 1 = January to 12 = December
-     * @return
-     */
+    // @param month 1 = January to 12 = December
     public static int task2HowManySundays(int year, int month) {
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy, E, h:mm:s");
         int howManySundays = 0;
-        int howManyDaysInAMonth = 0;
 
         GregorianCalendar cale = new GregorianCalendar(year, month - 1, 1);
         SimpleDateFormat sdf2 = new SimpleDateFormat("MMMM");
         System.out.println("Month: " + sdf2.format(cale.getTime()) + " of " + year);
-        howManyDaysInAMonth = cale.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int howManyDaysInAMonth = cale.getActualMaximum(Calendar.DAY_OF_MONTH);
 
         for (int dayCounter = 1; dayCounter <= howManyDaysInAMonth; dayCounter++) {
             cale.set(year, month - 1, dayCounter);
@@ -51,5 +46,30 @@ public class CalendarTasks {
         System.out.println(howManySundays + " Sundays");
 
         return howManySundays;
+    }
+
+     // @param month 1 = January to 12 = December
+    public static int task3WorkdaysInAMonth(int year, int month) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy, E, h:mm:s");
+        int workDays = 0;
+
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, 1);
+
+        int howManyDaysInAMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        for (int dayCount = 0; dayCount <= howManyDaysInAMonth; dayCount++) {
+            calendar.set(year, month - 1, dayCount);
+
+            if ((calendar.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY)
+                    && (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY)) {
+                // System.out.println(sdf.format(calendar.getTime()));    // Print the whole month
+                workDays++;
+            }
+        }
+
+        System.out.println("Month: " + sdf.format(calendar.getTime()) + " of " + year);
+        System.out.println("Workdays: " + workDays);
+        return workDays;
     }
 }
