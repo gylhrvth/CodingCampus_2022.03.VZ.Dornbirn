a01b64f4a8888831ae6de7917b9997964ee5225f
lukas <lukas.aichbauer@gmail.com>
2022-04-06 12:03:48 +0200

    - Calendar exercise. - Colors.

---
+++ src/main/java/lukas/week4/day3/Color.java
@@ -0,0 +1,14 @@
+package lukas.week4.day3;
+
+public class Color {
+    public static final String RESET = "\u001B[0m";
+    public static final String[] COLORS = new String[]{
+            "\u001B[31m",
+            "\u001B[32m",
+            "\u001B[33m",
+            "\u001B[34m",
+            "\u001B[35m",
+            "\u001B[36m",
+            "\u001B[37m"
+    };
+}
