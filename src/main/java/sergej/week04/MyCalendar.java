a034f3f831cfb00c678bf48d3988732430ed02e5
Sergej <orasch.s@live.at>
2022-04-06 11:26:43 +0200

    Added My New Calendar

--- src/main/java/sergej/week04/MyCalendar.java
+++ src/main/java/sergej/week04/MyCalendar.java
@@ -1,5 +1,7 @@
 package sergej.week04;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -7,49 +9,74 @@ public class MyCalendar {
 
     public static void newCalendar(int year, int month) {
 
-        GregorianCalendar gc = new GregorianCalendar(year, month - 1, 1);
+        GregorianCalendar gc = new GregorianCalendar(year, month - 1,1);
 
         int daysInMonth = gc.getActualMaximum(Calendar.DAY_OF_MONTH);
 
-        System.out.println("|MO| |DI| |MI| |DO| |FR| |SA| |SO|");
+        System.out.println(new SimpleDateFormat("yyyy MMMM").format(gc.getTime()));
+
+        System.out.println("| MO | | DI | | MI | | DO | | FR | | SA | | SO |");
 
         int isSunday = Calendar.SUNDAY;
+        int isMonday = Calendar.MONDAY;
         String initialSpace = "";
 
         if (isSunday == gc.get(Calendar.DAY_OF_WEEK)) {
             for (int i = 0; i < 6; i++) {
-                initialSpace += "|  |" + " ";
+                initialSpace += "|    |" + "  ";
             }
         } else {
             for (int i = 0; i < gc.get(Calendar.DAY_OF_WEEK) - 2; i++) {
-                initialSpace += "|  |" + " ";
+                initialSpace += "|    |" + " ";
             }
         }
         System.out.print(initialSpace);
 
+
+        LocalDate currentdate = LocalDate.now();
+
+        int currentday = currentdate.getDayOfMonth();
+
         for (int day = 1; day <= daysInMonth; day++) {
-            gc.set(year, month - 1, day);
+            gc.set(year, month - 1,day);
             int dayofmonth = gc.get(Calendar.DAY_OF_MONTH);
             int dayofweek = gc.get(Calendar.DAY_OF_WEEK);
 
-            if (dayofweek - 1 == Calendar.SUNDAY) {
-                System.out.println();
-            }
 
             if (day < 10) {
-                System.out.print("| " + dayofmonth + "| ");
-                continue;
+                if (currentday == dayofmonth) {
+                    System.out.print("| " + dayofmonth + " *| ");
+                } else {
+                    System.out.print("|  " + dayofmonth + " | ");
+                }
+            } else {
+                if (currentday == dayofmonth) {
+                    System.out.print("| " + dayofmonth + "*| ");
+                } else {
+                    System.out.print("| " + dayofmonth + " | ");
+                }
             }
-            System.out.print("|" + dayofmonth + "| ");
-
+            if (dayofweek  == Calendar.SUNDAY) {
+                System.out.println();
+            }
         }
 
-    }
+        int lastday = gc.getActualMaximum(Calendar.DATE);
+        gc.set(Calendar.DATE, lastday);
+        int lastDay1 = gc.get(Calendar.DAY_OF_WEEK);
+        String postspace = "";
 
+
+        if (isSunday != gc.get(Calendar.DAY_OF_WEEK)) {
+            for (int i = 0; i < 8 - lastDay1; i++) {
+                postspace += "|    |" + " ";
+            }
+        }
+        System.out.print(postspace);
+    }
 
     public static void main(String[] args) {
 
         newCalendar(2022, 4);
-
     }
 }
