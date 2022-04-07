bd840e54b95ac0887439cedbbee463ee1c6a48e3
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-04-07 12:32:16 +0200

    "Reverse String V2 (faster)" completed.

--- src/main/java/murat/week4/day4/ReverseString.java
+++ src/main/java/murat/week4/day4/ReverseString.java
@@ -11,9 +11,23 @@ public class ReverseString {
         System.out.println("Please enter your word to reverse");
         String userText = sc.nextLine();
 
+        long startNS = System.nanoTime();
         // Print user text reversely
-        System.out.println(reverseString(userText));
+        String reversed = reverseString(userText);
+        long endNS = System.nanoTime();
+        System.out.println(reversed);
+        System.out.println("Murat reverse took: " + (endNS - startNS) / 1000000f + "ms");
 
+        String bigString = new String(new byte[100000]);
+        long start = System.currentTimeMillis();
+        reverseString(bigString);
+        long end = System.currentTimeMillis();
+        System.out.println("MSL Reverse took: " + (end - start) + "ms.");
+
+        start = System.currentTimeMillis();
+        reverseStringV2(bigString);
+        end = System.currentTimeMillis();
+        System.out.println("MSL ReverseV2 took: " + (end - start) + "ms.");
     }
 
     public static String reverseString(String aString) {
@@ -25,5 +39,18 @@ public class ReverseString {
             aString = newString;
         }
         return aString;
+    }
+
+    public static String reverseStringV2(String aString) {
+
+        char[] charsOfAString = aString.toCharArray();
+
+        for (int i = 0, j = aString.length() - 1; i < j; i++, j--) {
+            char first = charsOfAString[i];
+            charsOfAString[i] = charsOfAString[j];
+            charsOfAString[j] = first;
+        }
+
+        return new String(charsOfAString);
     }
 }
