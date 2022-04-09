a945077e1f904a27924e29748584029ef9a6cc71
PhilippRB <philbert@gmx.at>
2022-04-09 11:09:50 +0200

    File renamed

--- src/main/java/philipp/Week04/DateTimeExercises.java
+++ src/main/java/philipp/Week04/DateTimeExercises.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 
 import java.util.GregorianCalendar;
 
-public class DateTimeExersices {
+public class DateTimeExercises {
 
     public static void main(String[] args) {
         System.out.println("Next Sunday is:");
