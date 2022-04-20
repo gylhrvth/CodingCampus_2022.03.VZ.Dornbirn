04dbb4da2c7e875e72a1dfe10ad58ed4822185f4
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-04-20 12:12:24 +0200

    Display A Table completed.

--- src/main/java/murat/week5/day3/DisplayAsATable.java
+++ src/main/java/murat/week5/day3/DisplayAsATable.java
@@ -9,12 +9,58 @@ public class DisplayAsATable {
         String[] place = {"Wien", "Schwarzach", "Wiener Neudorf", "Sankt Pölten", "Sankt Pölten"};
         float[] distanceFromCapital = {0f, 654.4f, 12.457634366f, 120.0f, 119.9999f};
 
-        System.out.println("---------------------------------------------------------------------");
-        for (int i = 0; i < 5; i++) {
-            System.out.printf("|%-16s |%-13s |%-3d |%-15s |%-13.9f|%n", firstName[i], lastName[i], age[i], place[i], distanceFromCapital[i]);
-            System.out.println("---------------------------------------------------------------------");
+        int lengthFirstName = getMaxString(firstName);
+        int lengthLastName = getMaxString(lastName);
+        int lengthAge = getMaxInt(age);
+        int lengthPlace = getMaxString(place);
+        int lengthDist = getMaxFloat(distanceFromCapital);
 
+        //System.out.println(lengthFirstName + " " + lengthLastName + " " + lengthAge + " " + lengthPlace + " " + lengthDist);
+
+        System.out.println("-------------------------------------------------------------------");
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("| %" + (-(lengthFirstName + 1)) + "s | %" + (-(lengthLastName + 1)) +
+                    "s | %" + (lengthAge + 1) + "d | %" + (-(lengthPlace + 1)) +
+                    "s |%" + (lengthDist + 3) + ".1f|%n", firstName[i], lastName[i], age[i], place[i], distanceFromCapital[i]);
         }
+
     }
 
+    public static int getMaxString(String[] text) {
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < text.length; i++) {
+            if (text[i].length() > max) {
+                max = text[i].length();
+            }
+        }
+        return max;
+    }
+
+    public static int getMaxInt(int[] number) {
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < number.length; i++) {
+
+            if (number[i] > max) {
+                max = number[i];
+            }
+        }
+
+        return (int) (Math.log10(max) + 1);
+    }
+
+    public static int getMaxFloat(float[] distance) {
+
+        float max = Float.MIN_VALUE;
+
+        for (int i = 0; i < distance.length; i++) {
+            if (distance[i] > max) {
+                max = distance[i];
+            }
+        }
+        return (int) (Math.log10(max) + 1);
+    }
 }
