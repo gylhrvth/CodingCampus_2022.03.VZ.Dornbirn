504e9ebe824a53327ee88f12cc4857c4658edfa1
Razvan Zah <zah_razvan@yahoo.com>
2022-04-22 13:32:16 +0200

    EasterHoliday

---
+++ src/main/java/zah/week05/Split.java
@@ -0,0 +1,8 @@
+package zah.week05;
+
+public class Split {
+    public static void main(String[] args) {
+        String text = "Heute wird ein guter Tag! Heute wird der beste Tag überhaupt! Heute wird ein spitzen super Tag!";
+    }
+
+}
