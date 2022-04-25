d92d25c24554f41e5ac38d42d785ea6ff0b244db
Gyula Horvath <gylhrvth@gmail.com>
2022-04-25 09:34:42 +0200

    Recursive Functions with indentation

--- src/main/java/gyula/week05/Permutation.java
+++ src/main/java/gyula/week05/Permutation.java
@@ -2,7 +2,7 @@ package gyula.week05;
 
 public class Permutation {
     public static void main(String[] args) {
-
+        printPermutations("", "", "ABCDE");
     }
 
     /**
@@ -18,7 +18,18 @@ public class Permutation {
      * Prefix, Rest
      *
      */
-    public static void printPermutations(String prefix, String rest){
-
+    public static void printPermutations(String indentation, String prefix, String rest){
+        System.out.println(indentation + "printPermutations(" + prefix + ", " + rest + ")");
+        if (rest.length() == 0){
+            //System.out.println(prefix); // Result
+        } else {
+            for (int i = 0; i < rest.length(); i++) {
+                printPermutations(
+                        indentation + "  ",
+                        prefix + rest.substring(i, i+1),
+                         rest.substring(0, i) + rest.substring(i+1)
+                        );
+            }
+        }
     }
 }
