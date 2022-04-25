64384babce93ec4db122d5d07d67ebf728782cac
Arda Yalcin <wsyalcin@gmail.com>
2022-04-25 14:43:20 +0200

    Fixed nullpointerexception

--- src/main/java/arda/week06/day01/FileCountAndSumRecursive.java
+++ src/main/java/arda/week06/day01/FileCountAndSumRecursive.java
@@ -4,29 +4,37 @@ import java.io.File;
 
 public class FileCountAndSumRecursive {
     public static void main(String[] args) {
-        File file = new File("c:\\");
+        File file = new File("C:\\Users\\DCV\\IdeaProjects\\CodingCampus_2022.03.VZ.Dornbirn\\src\\main\\java");
        System.out.println("amount of files: " + countFiles(file));
-       //System.out.println("sum of file sizes: " + sumOfFiles(file));
+       System.out.println("sum of file sizes: " + sumOfFiles(file));
     }
     public static int countFiles(File file){
-        File[] fileList = file.listFiles();
         int counter = 0;
-        for (int i = 0; i < fileList.length; i++) {
-            if (!fileList[i].isDirectory()){
-                counter++;
-            }else{
+
+        if (file.isFile()){
+            return 1;
+        }
+
+        File[] fileList = file.listFiles();
+        if (fileList != null) {
+            for (int i = 0; i < fileList.length; i++) {
                 counter += countFiles(fileList[i]);
             }
+        } else {
+            System.out.println(file.getAbsolutePath() + " is not readable.");
         }
         return counter;
     }
-    public static int sumOfFiles(File file){
-        File[] fileList = file.listFiles();
+    public static long sumOfFiles(File file){
         int sum = 0;
-        for (int i = 0; i < fileList.length; i++) {
-            if (!file.isDirectory()){
-                sum += fileList[i].length();
-            }else{
+
+        if (file.isFile()){
+            return file.length();
+        }
+
+        File[] fileList = file.listFiles();
+        if (fileList != null) {
+            for (int i = 0; i < fileList.length; i++) {
                 sum += sumOfFiles(fileList[i]);
             }
         }
