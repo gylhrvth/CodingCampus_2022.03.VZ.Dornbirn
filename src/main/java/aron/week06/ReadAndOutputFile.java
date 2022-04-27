bb9af4d97f8bd1675d6ab39a2a6fc83de45ebdb7
Aron Redl <Crenkh@gmx.at>
2022-04-27 13:27:30 +0200

    Read File op

--- src/main/java/aron/week06/ReadAndOutputFile.java
+++ src/main/java/aron/week06/ReadAndOutputFile.java
@@ -2,19 +2,36 @@ package aron.week06;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ReadAndOutputFile {
 
     public static void main(String[] args) {
 
-        FileReader fr = new FileReader("assets/tmp/aron.txt");
-        BufferedReader br = new BufferedReader(fr);
-
-        int i;
-        while ((i = br.read()) != -1) {
-            System.out.print((char) i);
+        try {
+            FileReader fr = new FileReader("assets/tmp/aron.txt");
+            BufferedReader br = new BufferedReader(fr);
+            while (br.ready()){
+                String line = br.readLine();
+                System.out.println(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        br.close();
-        fr.close();
+
+
+//        try {
+//            FileReader fr = new FileReader("assets/tmp/aron.txt");
+//            BufferedReader br = new BufferedReader(fr);
+//
+//            int i = br.read();
+//            while (i != -1) {
+//                System.out.print((char) i);
+//                i = br.read();
+//            }
+//            br.close();
+//            fr.close();
+//        } catch (IOException ioe){ }
     }
 }
