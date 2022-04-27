77b612457aa2a9faaa8c0c9f300f684b38803cb9
Arda Yalcin <wsyalcin@gmail.com>
2022-04-27 10:46:03 +0200

    Log task works now.

--- src/main/java/arda/week06/day02/Log.java
+++ src/main/java/arda/week06/day02/Log.java
@@ -12,26 +12,32 @@ import java.util.GregorianCalendar;
 
 public class Log {
     public static void main(String[] args) {
-        String date = dateTime();
-        log(date, 2, ">:(");
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j < 10; j++) {
+                log(2, "Hello World! 123546788 sdfsfghjj fdfwer erwrwer egdgdfg>:(");
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static void log(String date, int severity, String message) {
-        boolean flag = false;
-        while (!flag) {
-            try {
-                PrintStream ps = new PrintStream(new FileOutputStream("C:/intellij/LogClass/log.txt", true));
-                String[] levels = {"ERROR", "WARNING", "INFO"};
-                if (severity < 1 || severity > levels.length) {
-                    severity = levels.length;
-                }
-                ps.printf("%s %s: %s %n", date, levels[severity - 1], message);
-                flag = fileSizeLimit();
-
-
-            } catch (FileNotFoundException fnfe) {
-                fnfe.printStackTrace();
+    public static void log(int severity, String message) {
+        String logPath = "C:/intellij/LogClass/log.txt";
+        fileSizeLimit(logPath);
+        try {
+            PrintStream ps = new PrintStream(new FileOutputStream(logPath, true));
+            String[] levels = {"ERROR", "WARNING", "INFO"};
+            if (severity < 1 || severity > levels.length) {
+                severity = levels.length;
             }
+            ps.printf("%s %s: %s %n", dateTime(), levels[severity - 1], message);
+            ps.close();
+
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
         }
     }
 
@@ -41,16 +47,20 @@ public class Log {
         return sdf.format(gc.getTime());
     }
 
-    public static boolean fileSizeLimit() {
-        File file = new File("C:/intellij/LogClass/log.txt");
-        File rename = new File("C:/intellij/LogClass/log.old.txt");
-        byte size = 111;
+    public static boolean fileSizeLimit(String logPath) {
+        File file = new File(logPath);
+        File rename = new File(logPath + ".old");
         boolean flag = false;
-        if (file.length() > size) {
+        if (file.isFile() && file.length() > 100000L) {
+            if (rename.exists()){
+                rename.delete();
+            }
             flag = file.renameTo(rename);
-        }
-        if (flag) {
-            System.out.println("file renamed due its size being over the size limit");
+            if (flag) {
+                System.out.println("file renamed due its size being over the size limit");
+            } else {
+                System.out.println("file rename has failed");
+            }
         }
         return flag;
     }
