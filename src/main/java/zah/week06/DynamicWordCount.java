8c02d3d2952b0302978926512aecf1a0bd8cd7e3
Razvan Zah <zah_razvan@yahoo.com>
2022-04-28 14:37:29 +0200

    Writefile

---
+++ src/main/java/zah/week06/DynamicWordCount.java
@@ -0,0 +1,69 @@
+package zah.week06;
+
+import data.Texts;
+
+import java.io.*;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class DynamicWordCount {
+    public static void main(String[] args) {
+        Reader reader = new InputStreamReader(Objects.requireNonNull(DynamicWordCount.class.getClassLoader().getResourceAsStream("txt/simpleText.txt")));
+        InputStream inputStream = Objects.requireNonNull(DynamicWordCount.class.getClassLoader().getResourceAsStream("txt/simpleText.txt"));
+        dynamicCount(reader);
+        wordCount(inputStream);
+
+    }
+
+    public static void dynamicCount(Reader reader) {
+        int counter = 0;
+        System.out.println("Enter a Word: ");
+        String word = new Scanner(System.in).next().toLowerCase();
+
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(reader);
+            while (bufferedReader.ready()) {
+                String text = bufferedReader.readLine();
+
+                for (String tmp : text.split(" ")) {
+                    if (tmp.toLowerCase().equals(word)) {
+                        counter++;
+                    }
+                }
+            }
+            System.out.println("Word is: " + counter + " times");
+
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    //noop
+                }
+            }
+        }
+    }
+    public static void wordCount(InputStream inputStream) {
+        int count = 0;
+        System.out.println("Gib dein Wort ein: ");
+        String word = new Scanner(System.in).next().toLowerCase();
+
+        Scanner sc = new Scanner(inputStream);
+        while (sc.hasNext()) {
+            String text = sc.next();
+            if (text.toLowerCase().equals(word)) {
+                System.out.printf("%-19s %5d\n", text, count);
+                count++;
+            }
+
+//            System.out.printf("%-19s %5d\n", text, count);
+        }
+        System.out.println("Es sind " + count + " Wörter mit " + "'" + word + "'" + " im Text");
+
+    }
+}
+
