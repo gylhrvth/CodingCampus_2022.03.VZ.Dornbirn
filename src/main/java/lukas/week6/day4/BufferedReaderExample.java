b96185e761e7c836acc9f11b3ee2f6d5c694b945
lukas <lukas.aichbauer@gmail.com>
2022-04-28 12:39:49 +0200

    - Print file from the internet.

--- src/main/java/lukas/week6/day4/BufferedReaderExample.java
+++ src/main/java/lukas/week6/day4/BufferedReaderExample.java
@@ -1,15 +1,29 @@
 package lukas.week6.day4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
+import java.net.URL;
 import java.util.Objects;
 
 public class BufferedReaderExample {
     public static void main(String[] args) {
         Reader reader = new InputStreamReader(Objects.requireNonNull(BufferedReaderExample.class.getClassLoader().getResourceAsStream("txt/simpleText.txt")));
         printData(reader);
+
+        InputStream inputStream = null;
+        try {
+            inputStream = new URL("https://docs.oracle.com/javase/7/docs/api/java/io/InputStream.html").openStream();
+            printData(new InputStreamReader(inputStream));
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        } finally {
+            if(inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private static void printData(Reader reader) {
