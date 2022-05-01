769e563bc520e72bde8d23bb5a08a4858ee1d97d
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-05-01 22:36:03 +0200

    Read User Input With HasNextInt added.

--- src/main/java/murat/muratStandardLibrary/MuratInput.java
+++ src/main/java/murat/muratStandardLibrary/MuratInput.java
@@ -8,11 +8,12 @@ public class MuratInput {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Please enter a number!");
-        System.out.println(readANumberFromUser(scan, 1, 10));
+        //System.out.println(readANumberFromUserWithMaxValue(scan, 1, 10));
+        System.out.println(readANumberFromUserWithMaxValue(scan, 1, 10));
 
     }
 
-    public static int readANumberFromUser(Scanner sc, int lowerLimit, int upperLimit) {
+    public static int readANumberFromUserWithMaxValue(Scanner sc, int lowerLimit, int upperLimit) {
 
         int number = Integer.MAX_VALUE;
         while (number == Integer.MAX_VALUE) {
@@ -29,5 +30,24 @@ public class MuratInput {
             }
         }
         return number;
+    }
+
+    public static int readUserNumberWithHasNextInt(int lowerLimit, int upperLimit) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the column number!");
+        while (true) {
+            if (sc.hasNextInt()) {
+                int col = sc.nextInt();
+                if (col < lowerLimit || col > upperLimit) {
+                    System.out.println("A number between [" + lowerLimit + "," + upperLimit + "] is required. Please try again.");
+                } else {
+                    return col;
+                }
+            } else {
+                System.out.println("An integer is required!");
+                sc.nextLine();
+            }
+        }
     }
 }
