19ba5aa416bef8fa2d46f5ed3162fbbce3a9df13
Cemil Dincer <cemil_dincer@gmx.at>
2022-05-02 08:34:36 +0200

    String CSV 2 Dim

--- src/main/java/cemil/week07/Day5CSVReadDisplay.java
+++ src/main/java/cemil/week07/Day5CSVReadDisplay.java
@@ -4,33 +4,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.Arrays;
 import java.util.Objects;
 
 public class Day5CSVReadDisplay {
+
     public static void main(String[] args) {
         Reader reader = new InputStreamReader(Objects.requireNonNull(Day5CSVReadDisplay.class.getClassLoader().getResourceAsStream("csv/sales_100.csv")));
-
         String read = readString(reader);
-        String[] lines = splitLines(read);
+        String[][] matrix = splitList(read);
+        int[] longest = longest(matrix);
+        listBuilder(matrix,longest);
 
-        for (String line : lines) {
-            System.out.println("Zeile: " + line);
+    }
+
+    public static int[] longest(String[][] matrix) {
+        int[] longest = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                String word = matrix[i][j];
+                int cur = longest[j];
+                if (word.length() > cur) {
+                    longest[j] = word.length();
+                }
+            }
         }
-        String[] col = splitColum(read);
-
+        return longest;
     }
-    public static String[] splitColum (String text){
-       String[] col = text.split(System.lineSeparator());
-       String[][] arr = new String[col.length][];
+
+    public static void listBuilder(String[][] list,int[] longest) {
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                System.out.printf("%-" + longest[j] + "s | ", list[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static String[][] splitList(String text) {
+        String[] lines = text.split(System.lineSeparator());
+        String[][] arr = new String[lines.length][];
         for (int i = 0; i < arr.length; i++) {
-
-
-        }return col;
-    }
-
-    public static String[] splitLines(String text) {
-        return text.split(System.lineSeparator());
+            arr[i] = lines[i].split(",");
+        }
+        return arr;
     }
 
     public static String readString(Reader re) {
