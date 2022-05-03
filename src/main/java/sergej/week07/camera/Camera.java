795cf5a2e799f1acb81f856a5b2c927a83ca07a1
Sergej <orasch.s@live.at>
2022-05-03 15:34:05 +0200

    Added  camera + memorycard class

--- src/main/java/sergej/week07/camera/Camera.java
+++ src/main/java/sergej/week07/camera/Camera.java
@@ -4,41 +4,75 @@ public class Camera {
 
     private String model;
     private String manufact;
-    private int focalMin;
-    private int focalMax;
     private int megapixel;
     private MemoryCard sd;
+    private Objective ob;
 
-    public Camera(String model, String manufact, int focalMin, int focalMax, int megapixel) {
+    public Camera(String model, String manufact, int megapixel) {
         this.model = model;
         this.manufact = manufact;
-        this.focalMin = focalMin;
-        this.focalMax = focalMax;
         this.megapixel = megapixel;
         this.sd = null;
+        this.ob = null;
     }
 
     @Override
     public String toString() {
         return "Model: " + model + "\n" +
                 "Hersteller: " + manufact + "\n" +
-                "Brennweite minimum: " + focalMin + "mm" + "\n" +
-                "Brennweite maximum: " + focalMax + "mm" + "\n" +
-                "Megapixel: " + megapixel + "mb" + "\n";
+                "Brennweite minimum: " + (ob != null ? ob.getFocalMin() : "Kein Objektiv!!") + "mm" + "\n" +
+                "Brennweite maximum: " + (ob != null ? ob.getFocalMax() : "Kein Objektiv!!") + "mm" + "\n" +
+                "Megapixel: " + megapixel + "mb" + "\n" +
+                "Objektiv: " + (ob != null ? ob.getManufactur() : "Kein Objektiv!!") + "\n";
     }
 
     public void takePhoto() {
         if (sd != null) {
             System.out.println("Click");
             sd.reserveSpace(0.3f);
-            System.out.println("Space left: " + sd.getFreeSpace());
+            System.out.printf("Space left: %.2f mb\n", sd.getFreeSpace());
         } else {
             System.out.println("No memory card. Take photo is not allowed.");
         }
 
     }
 
+    public void checkMemory() {
+        if (sd != null) {
+            System.out.println("Space on Memory left: " + sd.getFreeSpace());
+        } else {
+            System.out.println("NO card to check!");
+        }
+    }
+
+    public void swapMem(MemoryCard mc) {
+        this.sd = mc;
+
+    }
+
+    public void swapOb(Objective ob) {
+        if (ob == null) {
+            if (this.ob != null) {
+                this.ob.setCam(null);
+            }
+            this.ob = null;
+        } else {
+            if (ob.getCam() != null) {
+                System.out.println("Objectiv is allready mounted >>" + ob.getCam());
+                ob.getCam().swapOb(null);
+                System.out.println("Objectiv has been removed from the other camera");
+                this.ob = ob;
+                ob.setCam(this);
+            }
+            this.ob = ob;
+            ob.setCam(this);
+        }
+
+    }
+
+
     public void setSd(MemoryCard sd) {
         this.sd = sd;
     }
+
 }
