9e78d8eafe82dfef1fb7a0cb81b5b04d45f63552
Murat Kücüktepe <muratkucuktepe@gmail.com>
2022-05-04 11:43:04 +0200

    Vector Examples

---
+++ src/main/java/murat/week7/day3/Main.java
@@ -0,0 +1,28 @@
+package murat.week7.day3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car ford = new Car("Ford", "Mustang", 200, 60, 1500);
+
+        System.out.println(ford);
+        System.out.println("-----");
+        driveCar(120, ford);
+    }
+
+    private static void driveCar(int kilometer, Car aCar) {
+
+        float totalFuelUsage = kilometer * Car.fuelUsagePerKm(aCar.getkW(), aCar.getWeight());
+        aCar.setFuelTank(aCar.getFuelTank() - totalFuelUsage);
+        System.out.println("Remaining fuel: " + aCar.getFuelTank());
+
+        if (aCar.getFuelTank() == 0) {
+            System.out.println("Fuel tank is empty!");
+        }else{
+            System.out.println("You drove " + (aCar.getFuelTank() - totalFuelUsage) );
+        }
+
+
+    }
+}
