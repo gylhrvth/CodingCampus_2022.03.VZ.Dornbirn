fecb93fbc10483e5f28140e688665411db2a986b
Gyula Horvath <gylhrvth@gmail.com>
2022-05-04 09:35:43 +0200

    Vector functions

--- src/main/java/gyula/week07/VectorExample.java
+++ src/main/java/gyula/week07/VectorExample.java
@@ -5,29 +5,41 @@ import java.util.Vector;
 public class VectorExample {
     public static void main(String[] args) {
         Vector<String> v = new Vector<>();
-        v.add("Hello");
-        v.add("World!");
-        v.add("Alpha");
 
-        System.out.println(v.size());
-        System.out.println(v.capacity()); // We don't need it...
-        for (String s: v){
-            System.out.println(s);
+        v.add("Apfel");
+        v.add("Birne");
+        for (int i = 0; i < 10; i++) {
+            v.add("Korb " + i);
         }
-        System.out.println("-----------");
+        System.out.println(v);
+        v.remove("Birne");
+        System.out.println(v);
+        v.remove(1);
+        System.out.println(v);
+        v.add(1, "Banane");
+        System.out.println(v);
+
         for (int i = 0; i < v.size(); i++) {
             String s = v.get(i);
-            System.out.println(s);
+            System.out.println("==> " + s);
         }
-        v.add(1, "Beta");
+        System.out.println("________________________");
+        for (String s: v){
+            System.out.println("--> " + s);
+        }
+
+        v.set(2, "Korb 99");
         System.out.println(v);
-        System.out.println("indexOf: " + v.indexOf("Beta"));
-        System.out.println("contains " + v.contains("Alpha"));
-        v.remove("Beta");
-        System.out.println(v);
-        String first = v.remove(0);
-        System.out.println(first);
-        System.out.println(v);
+
+
+        System.out.println("size() is " + v.size() +
+                " capacity() " + v.capacity());
+
+        System.out.println("contains(BRRRRR): " + v.contains("BRRRRR"));
+        System.out.println("contains(Banane): " + v.contains("Banane"));
+
+        System.out.println("indexOf(BRRRRR): " + v.indexOf("BRRRRR"));
+        System.out.println("indexOf(Banane): " + v.indexOf("Banane"));
 
     }
 }
