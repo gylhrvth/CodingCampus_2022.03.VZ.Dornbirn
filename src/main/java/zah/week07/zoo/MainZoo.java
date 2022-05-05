e58e02243f50198cebbdb89a0b2d8152bdc9c8a0
Razvan Zah <zah_razvan@yahoo.com>
2022-05-05 15:36:24 +0200

    zoo

--- src/main/java/zah/week07/zoo/MainZoo.java
+++ src/main/java/zah/week07/zoo/MainZoo.java
@@ -4,11 +4,20 @@ public class MainZoo {
     public static void main(String[] args) {
         Zoo z = new Zoo("ZooTime",1986);
 
-        z.addEnclosure(new Enclosure("Aquarium"));
-       // z.addEnclosure(new Enclosure("Terrarium"));
-       // z.addEnclosure(new Enclosure("MankyEnclosure"));
+
+        Enclosure aqarium=z.addEnclosure("Aquarium");
+        Enclosure terrarium=z.addEnclosure("Terrarium");
+        Enclosure mankyEclosure=z.addEnclosure("MankyEclosure");
+
+        aqarium.addAnimal("Sergej","Forelle");
+        aqarium.addAnimal("Jhon", "Delfin");
+        terrarium.addAnimal("Jan","Crocodile");
+        mankyEclosure.addAnimal("Timo","Horse");
+
+
 
         System.out.println(z);
+
 
 
 
--- src/main/java/zah/week07/zoo/Zoo.java
+++ src/main/java/zah/week07/zoo/Zoo.java
@@ -3,27 +3,39 @@ package zah.week07.zoo;
 import java.util.Vector;
 
 public class Zoo {
+
     private String name;
     private int foundation;
     private Vector<Enclosure> enclosures;
 
-    public Zoo(String name, int founadtion) {
+    public Zoo(String name, int foundation) {
+
         this.name = name;
-        this.foundation = founadtion;
+        this.foundation = foundation;
         this.enclosures = new Vector<>();
+    }
+
+
+    public String toString(String id) {
+        String out = id + "";
+        out += name;
+        out += " " + foundation + "\n";
+        for (Enclosure enc : enclosures) {
+            out += enc.toString(id+"  ") + "\n";
+        }
+        return out;
     }
 
     @Override
     public String toString() {
-        return "├──Zoo:" + name + " gegründet " + foundation + "\n" +
-                    "├──" + enclosures +"\n"+
-
-
-                '}';
+        return toString("");
     }
 
-    public void addEnclosure(Enclosure e) {
+
+    public Enclosure addEnclosure(String name) {
+        Enclosure e = new Enclosure(name);
         enclosures.add(e);
+        return e;
     }
 }
 
