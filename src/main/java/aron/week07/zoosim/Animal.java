2ebbf7216f1f6a137b09aa4dc3e8550d616d3ea8
Aron Redl <Crenkh@gmx.at>
2022-05-09 10:54:48 +0200

    ZooSim OP

--- src/main/java/aron/week07/zoosim/Animal.java
+++ src/main/java/aron/week07/zoosim/Animal.java
@@ -1,23 +1,45 @@
 package aron.week07.zoosim;
 
+import arda.week07.zoosimulation.Enclosure;
+
 public class Animal {
     
     private  String name;
     private String genus;
-    private int quantity;
+    private Enclosure  cageOfAnimal;
 
-    public Animal(String name , String genus, int quantity){
+    public Animal(String name , String genus){
 
         this.name = name;
         this.genus = genus;
-        this.quantity =quantity;
+        this.cageOfAnimal = null;
     }
 
-//      if(animals.size() == maxSize) {
-//        throw new Exception("Enclosure is full!!!!!!!!!!!");
+    public String getGenus() {
+        return genus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCageOfAnimal(Enclosure cageOfAnimal) {
+        this. cageOfAnimal = cageOfAnimal;
+    }
+
+    public Enclosure getCageOfAnimal() {
+        return  cageOfAnimal;
+    }
 
     @Override
-    public String toString() {return "Animal{" + "name='" + name + '\'' + ", genus='" + genus + '\'' + ", quantity=" + quantity + '}';
-
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", genus='" + genus + '\'' +
+                ", cageOfAnimal=" + cageOfAnimal +
+                '}';
     }
 }
+
+
+
--- src/main/java/aron/week07/zoosim/Enclosure.java
+++ src/main/java/aron/week07/zoosim/Enclosure.java
@@ -1,23 +1,78 @@
 package aron.week07.zoosim;
 
+
+import arda.week07.zoosimulation.Zoo;
+
 public class Enclosure {
 
     private String name;
     private ClimateZone clima;
     private Areasize size;
     private Animal animal;
+    private Zoo cageOfZoo;
+
+
 
     public Enclosure(String name, ClimateZone clima, Areasize size, Animal animal){
 
         this.name = name;
         this.clima = clima;
         this.size = size;
-        this.animal = animal;
+        this.animal = null;
+        this.cageOfZoo = null;
+
     }
+
+    public ClimateZone getClima() {return clima;
+    }
+
+    public void setClima(ClimateZone clima) {
+        this.clima = clima;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+    public void setCageOfZoo() {
+        this.cageOfZoo = cageOfZoo;
+    }
+
+    public Zoo getCageOfZoo() {
+        return cageOfZoo;
+    }
+
+    public void addAnimal(Animal animal) {
+        if (this.animal == null) {
+            if (animal.getCageOfAnimal() == null) {
+                this.animal = animal;
+                animal.getCageOfAnimal();
+            } else {
+                System.out.println("Animal is already in " + animal.getCageOfAnimal());
+            }
+        } else {
+            System.out.println("Enclosure already has an Animal in it");
+        }
+    }
+
+    public void removeAnimal() {
+        if (animal != null) {
+            animal.setCageOfAnimal(null);
+            animal = null;
+        } else {
+            System.out.println("Cage is already empty");
+        }
+    }
+
+
 
     @Override
-
-    public String toString() {return "Enclosure{" + "name='" + name + '\'' + ", clima='" + clima + '\'' + ", size=" + size +  ", animal=" + animal + '}';
-
+    public String toString() {
+        return "\n"+
+               " {"+"name='" + name + '\'' +
+                ", clima=" + clima +
+                ", size=" + size +
+                ", animal=" + animal +
+                ", cageOfZoo=" + cageOfZoo + '}';
     }
 }
+
