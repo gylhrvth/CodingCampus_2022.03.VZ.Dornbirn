4bb9924d17bd48cdf2dafa643b51fa59404f5cab
Razvan Zah <zah_razvan@yahoo.com>
2022-05-12 10:27:33 +0200

    -HashMap

---
+++ src/main/java/zah/week07/hashMap/HashMapCoutLetters.java
@@ -0,0 +1,20 @@
+package zah.week07.hashMap;
+
+import java.util.HashMap;
+
+public class HashMapCoutLetters {
+    public static void main(String[] args) {
+        String text = lukas.data.Texts.getSimpleText();
+        HashMap<Character, Integer> count = new HashMap<>();
+        for (int i = 0; i < text.length(); i++) {
+            Character nextChar = text.charAt(i);
+            if (count.containsKey(nextChar)){
+                count.put(nextChar, count.get(nextChar) + 1);
+            } else {
+                count.put(nextChar, 1);
+            }
+        }
+        System.out.println(count);
+    }
+
+}
