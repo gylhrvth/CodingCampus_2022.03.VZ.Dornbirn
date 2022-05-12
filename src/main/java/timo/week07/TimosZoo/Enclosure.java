73a180135078f3d079d3e1c2a4d191c4ca1e85bb
timokantner <timo.kantner@chello.at>
2022-05-12 10:17:23 +0200

    -improved zoo

--- src/main/java/timo/week07/TimosZoo/Enclosure.java
+++ src/main/java/timo/week07/TimosZoo/Enclosure.java
@@ -1,9 +1,13 @@
 package timo.week07.TimosZoo;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import java.util.Vector;
 
 public class Enclosure {
+    Random rand = new Random();
+
     private String name;
     private List<Animal> animals = new Vector<>();
     private int lastDayCleaned;
@@ -14,12 +18,19 @@ public class Enclosure {
     }
 
 
-    public String toString(String indention) {
-        String out = indention + name + " (Last day cleaned: " + lastDayCleaned + ")";
+    public String printString(String indention) {
+        String out = indention + name;
         for (Animal a : animals) {
-            out += "\n" + "│" + "        " + a.toString(indention);
+            out += "\n" + "│" + "        " + a.printString(indention);
         }
         return out;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\n" +
+                "Animals: " + animals +
+                ", cleaned: " + lastDayCleaned + " day/s ago";
     }
 
     public void setLastDayCleaned (int lastDayCleaned) {
@@ -27,20 +38,22 @@ public class Enclosure {
     }
 
 
-    public void cleanEnclosure(int currentDay) {
+    public boolean cleanEnclosure(int currentDay) {
         if (currentDay == lastDayCleaned) {
             // Nothing to do
-            System.out.println("Nothing to do");
+            System.out.println("\u001B[32m" + "This enclosure has already been cleaned and fed." + "\u001B[0m");
+            return false;
         } else {
             // Do cleaning
-            System.out.println("Do cleaning");
+            System.out.println("\u001B[31m" + "This enclosure needs to be cleaned and fed." + "\u001B[0m");
             lastDayCleaned = currentDay;
+            return true;
         }
     }
 
 
-    public Animal addAnimal(String name, String species) {
-        Animal a = new Animal(name, species);
+    public Animal addAnimal(String name, String species, Food food, int dailyNeed) {
+        Animal a = new Animal(name, species, food, dailyNeed);
         animals.add(a);
         return a;
     }
@@ -49,7 +62,21 @@ public class Enclosure {
         animals.remove(a);
     }
 
+    public List<Animal> getAnimals() {
+        return animals;
+    }
 
+    public void feedAnimals(Enclosure en) {
+        System.out.println(">> Proceeding to feed animals and clean enclosure <<");
+        for (int i = 0; i < en.getAnimals().size(); i++) {
+            System.out.print(" ".repeat(20));
+            System.out.println("Feeding: " + en.getAnimals().get(i));
+        }
+    }
+
+    public void getRandomAnimalofEn(Enclosure en) {
+        System.out.println("- Randomly watching: " + en.getAnimals().get(rand.nextInt(en.getAnimals().size())));
+    }
 }
 
 
---
+++ src/main/java/timo/week07/TimosZoo/Food.java
@@ -0,0 +1,49 @@
+package timo.week07.TimosZoo;
+
+public class Food {
+
+    private String name;
+    private String unit;
+    private int price;
+
+    public Food(String name, String unit, int price) {
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "name='" + name + '\'' +
+                ", unit='" + unit + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
