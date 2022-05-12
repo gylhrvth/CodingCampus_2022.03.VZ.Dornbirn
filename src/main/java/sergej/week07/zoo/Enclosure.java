c0c66ed92e441b1bc513236de7c992fcf7e25c3a
Sergej <orasch.s@live.at>
2022-05-12 11:40:07 +0200

    added bite attack chance (solved) infinity loop

--- src/main/java/sergej/week07/zoo/Enclosure.java
+++ src/main/java/sergej/week07/zoo/Enclosure.java
@@ -13,6 +13,16 @@ public class Enclosure {
     private int lastDayOfCleaning = 0;
     private static Random rand = new Random();
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public Enclosure(Zoo zoo, String name) {
         this.zoo = zoo;
         this.name = name;
@@ -58,37 +68,41 @@ public class Enclosure {
 
     public void simulate() {
         Vector<Animal> deadAnimals = new Vector<>();
-        for (Animal animal: animals){
-            if ((animal.getHealth() > 0) && (rand.nextInt(100) < animal.getAttackChance())){
+        for (Animal animal : animals) {
+            if ((animal.getHealth() > 0) && (rand.nextInt(100) < animal.getAttackChance())) {
+                if(animals.size() <= 1){
+                    break;
+                }
                 Animal victim = animal;
                 while (victim.equals(animal)) {
                     victim = animals.get(rand.nextInt(animals.size()));
+
                 }
                 if (victim.getHealth() > 0) {
                     int newHealth = Math.max(0, victim.getHealth() - animal.getDamage());
                     victim.setHealth(newHealth);
-                    System.out.printf("%1$s hat %2$s gebissen, %2$s's Restgesundheit ist %3$d%n", animal.getName(), victim.getName(), newHealth);
+                    System.out.printf(ANSI_RED + "%1$s" + ANSI_RESET + " hat " + "%2$s" + ANSI_RED + " gebissen" + ANSI_RESET + ", %2$s's Restgesundheit ist %3$d%n", animal.getName(), victim.getName(), newHealth);
                     if (newHealth <= 0) {
-                        System.out.printf("%s hat den Angriff nicht überlebt. Wird von dem Gehege entfernt.%n", victim.getName());
+                        System.out.printf(ANSI_PURPLE + "%s" + ANSI_RESET + " hat den Angriff nicht überlebt. Wird von dem Gehege entfernt.%n", victim.getName());
                         deadAnimals.add(victim);
                     }
                 }
             }
         }
-        for (Animal deadAnimal: deadAnimals){
+        for (Animal deadAnimal : deadAnimals) {
             animals.remove(deadAnimal);
         }
     }
 
     public Animal getAnimalWithMinHealth() {
         Animal result = null;
-        for (Animal animalInNeed: animals){
+        for (Animal animalInNeed : animals) {
             if (result == null) {
                 result = animalInNeed;
-            } else if (animalInNeed != null){
-                double relHealthOfResult = result.getHealth() / (double)result.getMaxHealth();
-                double relHealthOfAnimalInNeed = animalInNeed.getHealth() / (double)animalInNeed.getMaxHealth();
-                if (relHealthOfAnimalInNeed < relHealthOfResult){
+            } else if (animalInNeed != null) {
+                double relHealthOfResult = result.getHealth() / (double) result.getMaxHealth();
+                double relHealthOfAnimalInNeed = animalInNeed.getHealth() / (double) animalInNeed.getMaxHealth();
+                if (relHealthOfAnimalInNeed < relHealthOfResult) {
                     result = animalInNeed;
                 }
             }
