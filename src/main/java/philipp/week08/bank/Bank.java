5da9a1560d4d578907b6d5b1acc238947627dd9b
PhilippRB <philbert@gmx.at>
2022-05-14 18:18:29 +0200

    *bank exercise

--- src/main/java/philipp/week08/bank/Bank.java
+++ src/main/java/philipp/week08/bank/Bank.java
@@ -2,10 +2,10 @@ package philipp.week08.bank;
 
 public class Bank {
     private static Customer[] customers = {
-            new Customer("Customer 01", 156.52, true),
+            new Customer("Customer 01", 152.44, true),
             new Customer("Customer 02", 327.20, true),
             new Customer("Customer 03", 564.51, false),
-            new Customer("Customer 04", 222.22, false),
+            new Customer("Customer 04", 522.22, false),
             new Customer("Customer 05", 15.15, true),
             new Customer("Customer 06", 124.56, true),
             new Customer("Customer 07", 488.00, true),
@@ -14,8 +14,8 @@ public class Bank {
             new Customer("Customer 10", 164.88, false)};
 
     private static Counter[] counters = {
-            new Counter("Counter 01", 1000, true),
-            new Counter("Counter 02", 1000, false),
+            new Counter("Counter 01", 0, true),
+            new Counter("Counter 02", 1000, true),
             new Counter("Counter 03", 1000, true),
 //                new Counter("Counter 04",1000,true),
 //                new Counter("Counter 05", 1000, true)
@@ -27,19 +27,46 @@ public class Bank {
     }
 
     private static void bankSimulation(Customer[] customerInput, Counter[] counterInput) {
-        for (Customer c : customerInput) {
+        int counterBreakOrRefillMoney = 1;
+        for (Customer customer : customerInput) {
+//            System.out.println("-----");
+//            printCounters();
+            System.out.println("-----");
+            System.out.println(customer.getName());
+            for (Counter co : counterInput) {
+                if (co.getCounterRefill()) {
+                    co.setCounterRefillTime(co.getCounterRefillTime() + counterBreakOrRefillMoney);
+                }
+                if (co.getCounterBreak()) {
+                    co.setCounterBreakTime(co.getCounterBreakTime() + counterBreakOrRefillMoney);
+                }
+                co.checkIfOnBreakForRefill();
+                co.checkIfOnBreak();
+            }
             for (Counter counter : counterInput) {
-                if (c.getMoney() == 0) {
-                    continue;
+                if (customer.getMoney() == 0) {
+                    break;
                 }
                 if (counter.getCounterOpen()) {
-                    counter.moneyBusiness(c);
+                    if (counter.checkNoMoney(customer)) {
+                        counter.moneyBusiness(customer);
+                        counter.setCounterToBreak();
+                    }
                 } else {
-                    System.out.println(counter.getName() + " is occupied or closed.");
+                    System.out.println(customer.getName() + " wants to go to " + counter.getName() + ", but");
+                    System.out.println("\033[0;35m" + counter.getName() + " is closed." + "\033[0m");
                     System.out.println();
                 }
             }
         }
     }
+
+    private static void printCounters() {
+        for (Counter counter : counters) {
+            System.out.println(counter.toString());
+        }
+
+    }
 }
+
 
