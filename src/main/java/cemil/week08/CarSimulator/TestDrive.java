2b5bc316c6344d86715df16cbbac47815b6578fe
Cemil Dincer <cemil_dincer@gmx.at>
2022-05-18 09:57:39 +0200

    Ardas Cars laufen nicht

--- src/main/java/cemil/week08/CarSimulator/TestDrive.java
+++ src/main/java/cemil/week08/CarSimulator/TestDrive.java
@@ -1,14 +1,14 @@
 package cemil.week08.CarSimulator;
 
-import arda.week07.carsimulator.Car;
+
 
 public class TestDrive {
 
     public static void main(String[] args) {
-        Car[] cars = {new Car("BMW", "M50D", 400, 73, "Diesel", 1950),
-                new Car("AUDI", "A6", 326, 74, "Diesel", 1890),
-                new Car("LEXUS", "RS200", 200, 60, "Gasoline", 1360),
-                new Car("TOFAS", "DOGAN", 83, 50, "Gasoline", 1040)
-        };
-    }
+//        Car[] cars = {new Car("BMW", "M50D", 400, 73, "Diesel", 1950),
+//                new Car("AUDI", "A6", 326, 74, "Diesel", 1890),
+//                new Car("LEXUS", "RS200", 200, 60, "Gasoline", 1360),
+//                new Car("TOFAS", "DOGAN", 83, 50, "Gasoline", 1040)
+//        };
+   }
 }
